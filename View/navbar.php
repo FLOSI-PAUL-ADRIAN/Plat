6ffccb85d7a1ab66c6bdbe6f1ac1ded0a961f04d
badwolfover718 <47260542+badwolfover718@users.noreply.github.com>
2020-05-05 23:31:47 +0200

    Update navbar.php

--- View/navbar.php
+++ View/navbar.php
@@ -3,15 +3,19 @@
 <header>
 <navh class="navbar navbar-dark bg-dark">
 
-        <h1 class="text-success bg-dark"><a href="accueil.php">Home</h1>
+<a href="accueil.php"><h1 class="text-success bg-dark">Home</h1></a>
 
 
     <div class="container">
 
     <input class="form-control mr-0" type="search" placeholder="Nom du thème ou de la matière recherchée" aria-label="Search">
     <button class="btn btn-outline-info btn-sm my-0 ">Chercher</button>
-    <button class="btn btn-outline-light btn-sm my-0 "><a href="connexion.php">Connectez vous !!!</button>
-    <button class="btn btn-outline-light btn-sm my-0 "><a href="inscription.php">Inscription</button>
+    </div>
+    <div class="container">
+    <a href="connexion.php"><button class="btn btn-outline-light btn-sm my-0 ">Connectez vous !!!</button></a>
+    </div>
+    <div class="container">
+    <a href="inscription.php"><button class="btn btn-outline-light btn-sm my-0 ">Inscription</button></a>
     </div>
 
 </navh>
