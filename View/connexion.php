dc63dbe4c85b8a58d0352b2f46a7eedca58c8cf2
badwolfover718 <47260542+badwolfover718@users.noreply.github.com>
2020-05-05 13:09:06 +0200

    Update connexion.php

--- View/connexion.php
+++ View/connexion.php
@@ -1,81 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-<header>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Connexion</title>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-<button type="button" class="btn justify-content-center" data-toggle="modal" data-target="#myModal">
-                      Se connecter
-                </button>
-              </div>
-            </nav>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-            <div class="modal fade" id="myModal">
-              <div class="modal-dialog modal-sm">
-                <div class="modal-content">        
-                  <!-- Modal Header -->
-                  <div class="modal-header">
-                    <h4 class="modal-title">Connexion</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  </div>        
-                  <!-- Modal body -->
-                  <div class="modal-body">
-                      <form action="#" > <!-- Apellle le meme fichier -->
-                        <script type= "text/javascript" src="/script.js"></script>
-                          <div class="col-12 border6">
-                              <label for="login">Nom d'utilisateur</label>
-                              <input type="text" name="login" required="true">
-                  
-                              <label for="password">Mot de passe</label>
-                              <input type="password" name="password" required="true">
-                  
-                              <input type="submit" name="Valider" />        
-                          </div>
-                      </form> 
-                  </div>
-                  <!-- Modal footer -->
-                  <div class="modal-footer">
-                    <button type="button" class="btn justify-content-center" data-toggle="modal" data-target="#inscription">S'inscrire</button> 
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                  </div>
-                </div>
-              </div>
-            </div>
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/0ed05156d4.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <?php
+          include("navbar.php");
+        ?>
+</head>
+<style>
+body{
+    background: #67BE4B;
+}
+#container{
+    width:400px;
+    margin:0 auto;
+    margin-top:10%;
+}
+/* Bordered form */
+form {
+    width:100%;
+    padding: 30px;
+    border: 1px solid #f1f1f1;
+    background: #fff;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+#container h1{
+    width: 38%;
+    margin: 0 auto;
+    padding-bottom: 10px;
+}
+
+/* Full-width inputs */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+input[type=submit] {
+    background-color: #53af57;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+input[type=submit]:hover {
+    background-color: white;
+    color: #53af57;
+    border: 1px solid #53af57;
+}
 
 
-            <div class="modal fade" id="inscription">
-              <div class="modal-dialog modal-sm">
-                <div class="modal-content">
+</style>
+<body>
+<div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Connectez Vous !!!</h1>
                 
-                  <!-- Modal Header -->
-                  <div class="modal-header">
-                    <h4 class="modal-title">Inscription</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  </div>
-                  
-                  <!-- Modal body -->
-                  <div class="modal-body">
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
-                      <form method="post" action=adduser.php >
-                        <script type= "text/javascript" src="/script.js"></script>
-                          <div class="col-12 border6">
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                               <label for="username">Nom d'utilisateur</label>
-                              <input type="text" name="username" id="username" required="true">
-                              <label for="email">Adresse mail</label>
-                              <input type="text" name="email" id="email" required="true">
-                  
-                              <label for="password">Mot de passe</label>
-                              <input type="password" name="password" id="password" required="true">
-                  
-                              <input type="submit" name="Valider" id="valider">
-
-
-
-
-                      </form> 
-                  </div>
-                  <!-- Modal footer -->
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                  </div>
-       
-</header>
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
+            </form>
+        </div>
+</body>
