97ea3344a9db3f0270e514eeab8646a3d78d9d38
badwolfover718 <47260542+badwolfover718@users.noreply.github.com>
2020-05-05 13:09:18 +0200

    Update navbar.php

--- View/navbar.php
+++ View/navbar.php
@@ -1,31 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <header>
-    <navh class="navbar navbar-dark bg-dark">
+<navh class="navbar navbar-dark bg-dark">
 
-<h1 class="text-success bg-dark"><a href="accueil.php">Home</h1>
+        <h1 class="text-success bg-dark"><a href="accueil.php">Home</h1>
 
 
-<div class="container">
+    <div class="container">
 
     <input class="form-control mr-0" type="search" placeholder="Nom du thème ou de la matière recherchée" aria-label="Search">
     <button class="btn btn-outline-info btn-sm my-0 ">Chercher</button>
-
-</div>
-<!--<button class="btn btn-outline-danger btn-sm my-0 "><a href="Inscription.php">Inscription</button>-->
-
-<button class="btn btn-outline-light btn-sm my-0 "><a href="login.php">Se connecter</button>
-
-<?php
-            session_start();
-            if(!isset($_SESSION['name']))
-                echo '<a class="btn bg-lightred" href="login.php">Se connecter</a>';
-            ?>
+    <button class="btn btn-outline-light btn-sm my-0 "><a href="connexion.php">Connectez vous !!!</button>
+    <button class="btn btn-outline-light btn-sm my-0 "><a href="inscription.php">Inscription</button>
+    </div>
 
 </navh>
-<ul>
-<?php
-    include("connexion.php"); 
-?>
+
 
 </header> 
