88298f10d88f3ab7ba94ced59407f613e87b23c8
badwolfover718 <47260542+badwolfover718@users.noreply.github.com>
2020-05-06 20:08:10 +0200

    modif mathieu

--- View/navbar.php
+++ View/navbar.php
@@ -9,14 +9,14 @@
     <div class="container">
 
     <input class="form-control mr-0" type="search" placeholder="Nom du thème ou de la matière recherchée" aria-label="Search">
-    <button class="btn btn-outline-info btn-sm my-0 ">Chercher</button>
+    <button class="btn btn-outline-info btn-sm my-0  ">Chercher</button>
     </div>
-    <div class="container">
-    <a href="connexion.php"><button class="btn btn-outline-light btn-sm my-0 ">Connectez vous !!!</button></a>
-    </div>
-    <div class="container">
-    <a href="inscription.php"><button class="btn btn-outline-light btn-sm my-0 ">Inscription</button></a>
-    </div>
+    
+    <button class="btn btn-outline-light btn-sm my-0 pull-right"><a href="connexion.php">Connectez vous !!!</a></button>
+    
+  
+    <button class="btn btn-outline-light btn-sm my-0 pull-right"><a href="inscription.php">Inscription</a></button>
+    
 
 </navh>
 
